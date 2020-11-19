323daecd2890234b4fe8c1fc1fd4b441606f273f
J-Sparks <68030345+J-Sparks@users.noreply.github.com>
2020-11-18 19:00:38 -0600

    Update and rename accurecy_function.R to accuracy_function.R

--- accuracy_function.R
+++ accuracy_function.R
@@ -1,13 +1,18 @@
 #############################
-### ACCURECY ################
+### ACCURACY ################
 #############################
 
 # Using Navie Bayes classification
-
+library(caret)    # implementing with caret
 #Confusion Matrix (act\pred)
+confTier1 <- confusionMatrix(predict(aveGPANB_modelt1), aveGPAIndtTrainingSet$aveGPAInd) 
+### export the results
+p1 <-  predict(aveGPANB_modelt1, aveGPAIndtTrainingSet, type="prob")
+results1 <- cbind(aveGPAIndtTrainingSet,p1)
+
+# second way to determine accuracy
 confMatRank <- table(predict(allmodelNB), allDropoutTrainingSet$Drop1stYear)
 confMatRank 
-
 #Accurecy
 sum(diag(confMatRank)/sum(confMatRank)) # 0.8438356
 
