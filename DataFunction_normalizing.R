0490a97028717be8d4cd12ab8629704c3ecf956d
J-Sparks <68030345+J-Sparks@users.noreply.github.com>
2021-01-04 11:21:00 -0500

    Add files via upload

---
+++ DataFunction_normalizing.R
@@ -0,0 +1,5 @@
+### normalizing data
+mins <- apply(CSE_enr20201, 2, min)
+maxs <- apply(CSE_enr20201, 2, max)
+scaled.data <- scale(data, center=mins, scale=maxs-mins)
+scaled <- as.data.frame(scaled.data)
